8e0ed1fde58ce6b87559a9ed7f489bc74d39d154
gogbajbobo <grigoriev.maxim@gmail.com>
2018-03-06 15:38:14 +0300

    debug output correction

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const io = require('socket.io')();
 const debug = require('debug')('hbsocket:app');
 
 io.on('connection', client => {
-    debug(client + "connection");
+    debug(client.conn.id + " connection");
 });
 
 io.listen(3000);
